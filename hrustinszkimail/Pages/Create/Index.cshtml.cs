57c3146b66f357336c6d5141acce774956941706
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-02-02 12:08:46 +0100

    setup

---
+++ hrustinszkimail/Pages/Create/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hrustinszkimail.Pages;
+
+public class CreateModel : PageModel
+{
+
+    public void OnGet()
+    {
+
+    }
+}
